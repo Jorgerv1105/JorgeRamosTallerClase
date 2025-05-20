6f918f193ff9bd9936b4909262c7ff0f51dc4b18
Jorge Ramos <jorge.ramos.vasquez@udla.edu.ec>
2025-05-20 07:52:50 -0500

    Implementacion de AbsoluteLayout

--- JorgeRamosTallerClase/Views/AbsoluteLayout.xaml.cs
+++ JorgeRamosTallerClase/Views/AbsoluteLayout.xaml.cs
@@ -6,4 +6,8 @@ public partial class AbsoluteLayout : ContentPage
 	{
 		InitializeComponent();
 	}
+    private async void OnGoToGridLayout(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new GridLayout());
+    }
 }
